4fc500dc2174179a59ac7cd25e4fde9bf2b94ee5
Tim Gee <writetotimgee@gmail.com>
2020-05-25 22:11:19 +0100

    Re-add missing headers

--- src/main/java/com/trickl/flux/websocket/TextWebSocketFluxClient.java
+++ src/main/java/com/trickl/flux/websocket/TextWebSocketFluxClient.java
@@ -105,7 +105,7 @@ public class TextWebSocketFluxClient {
               URI transportUri = transportUriProvider.get();
               log.info("Connecting to " + transportUri);
               return webSocketClient
-                  .execute(transportUri, sessionHandler)
+                  .execute(transportUri, headers, sessionHandler)
                   .log("WebSocketClient", Level.FINER);
             })
         .doOnError(receiveSink::error)
